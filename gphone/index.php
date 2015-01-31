59872463019780ee7806806d19e94051f70c4887
Exho1 <nat.adams149@gmail.com>
2015-01-30 20:35:45 -0600

    Replaced index.php with index.html. Json!

--- gphone/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Ayy
-	$version = "1.2.3";
-	$description = "This is where a gPhone update description would be located at!";
-	$date = "1/27/2015";
-	
-	echo $version . "," . $description . "," . $date;
-?>
