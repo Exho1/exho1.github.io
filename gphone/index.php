20da87cdb3596a173a629348a5da43ca87d9ecc9
Exho1 <nat.adams149@gmail.com>
2015-01-30 20:17:06 -0600

    Added gPhone version page

---
+++ gphone/index.php
@@ -0,0 +1,8 @@
+<?php
+	// Ayy
+	$version = "1.2.3";
+	$description = "This is where a gPhone update description would be located at!";
+	$date = "1/27/2015";
+	
+	echo $version . "," . $description . "," . $date;
+?>
